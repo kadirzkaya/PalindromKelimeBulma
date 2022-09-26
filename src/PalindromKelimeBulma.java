fb3377895c5bccd20b01fb1c3bde7cb5e5ada0f6
kadirzkaya <bmkozkaya@gmail.com>
2022-09-26 11:57:31 +0300

    Palindromik kelime bulma

--- src/PalindromKelimeBulma.java
+++ src/PalindromKelimeBulma.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class PalindromKelimeBulma {
+import java.util.Scanner;
+
+public class PalindromKelimeBulma {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Bir kelime giriniz : ");
+        String word=sc.nextLine();
+        if (isPalindrome(word.toLowerCase())){
+            System.out.println(word + " kelimesi palindromiktir.");
+        }else {
+
+            System.out.println(word + " kelimesi palindromik değildir.");
+        }
+    }
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
